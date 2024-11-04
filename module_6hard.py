3f1c77e04df9538fee9cc048d99f0e3920ff964e
mnikodim <Marinenik@yandex.ru>
2024-11-04 13:29:02 +0300

    6hard_correction2

--- module_6hard.py
+++ module_6hard.py
@@ -14,22 +14,21 @@ class Figure:
     def __is_valid_color(self, color):
         if isinstance(color, str):
             try:
-                rgb = webcolors.name_to_rgb(color)
-                return rgb
+                webcolors.name_to_rgb(color)
+                return True
             except ValueError:
-                raise ValueError("Некорректное название цвета.")
+                return False
         elif isinstance(color, tuple) and len(color) == 3:
             if all(0<=value<=255 for value in color):
-                return color
-            else:
-                raise ValueError("RGB значения должны быть в диапазоне от 0 до 255.")
+                return True
         else:
-            raise ValueError("Цвет должен быть строкой или кортежем из трех значений.")
+            return False
 
     def set_color(self, r, g, b):
-        new_color = (r, g, b)
-        if self.__is_valid_color(new_color):
-            self.__color = new_color
+        if all(0<= value<=255 for value in(r, g, b)):
+            self.__color = (r, g, b,)
+        else:
+            ValueError('Цвет должен быть в диапазоне')
 
 
     def __is_valid_sides(self, *sides):  # принимает неграниченное количество сторон
@@ -37,6 +36,8 @@ class Figure:
             return sides
         else:
             raise ValueError("Все стороны должны быть положительными.")
+        if len(sides) != self.sides_count:
+            raise ValueError('Количество сторон должно соответствовать заявленным')
 
     def get_sides(self):
         return self.__sides # Возвращает значение сторон
@@ -56,13 +57,15 @@ class Circle(Figure):
 
     def __init__(self, color, radius, filled = False):
         super().__init__([radius], color, filled)
+        if radius < 0:
+            raise ValueError('Радиус должен быть положительным')
         self.__radius = radius
 
     def get_square(self):
         return math.pi*(self.__radius**2)
     def get_radius(self):
         return self.__radius
-    def set_radius(self):
+    def set_radius(self, radius):
         if radius > 0:
             self.__radius = radius
             self.set_sides(radius)
@@ -72,7 +75,10 @@ class Circle(Figure):
 class Triangle(Figure):
     sides_count = 3
     def __init__(self, sides, color, filled):
+        if len(sides) != self.sides_count:
+            raise ValueError('Треугольник должен иметь три стороны!')
         super().__init__(sides, color, filled)
+
 
     def get_square(self):
         if len(self.get_sides()) != self.sides_count:
@@ -96,7 +102,7 @@ class Cube(Figure):
     def get_length(self):
         return self.__length
 
-    def set_length(self):
+    def set_length(self, length):
         if length > 0:
             self.__length = length
             self.set_sides(length)
@@ -112,25 +118,23 @@ triangle1.set_color(100, 100, 255)
 print(triangle1.get_color())
 print(triangle1.get_square())
 
-circle1 = Circle((200, 200, 200), 10) # (Цвет, стороны)
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
 cube1 = Cube((222, 35, 130), 6)
 
 # Проверка на изменение цветов:
 circle1.set_color(55, 66, 77) # Изменится
 print(circle1.get_color())
-cube1.set_color(100, 70, 15) # Не изменится
+cube1.set_color(300, 70, 15) # Не изменится
 print(cube1.get_color())
 
 # Проверка на изменение сторон:
 cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
 print(cube1.get_sides())
-circle1.set_sides((15)) # Изменится
+circle1.set_sides(15) # Изменится
 print(circle1.get_sides())
 
 # Проверка периметра (круга), это и есть длина:
 print(len(circle1))
-print(circle1.get_square())
 
 # Проверка объёма (куба):
 print(cube1.get_volume())
-
